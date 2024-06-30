e684b78923eebe77b052e918a34d9f445849bc64
https-richardy <code.richardy@gmail.com>
2024-06-30 15:35:48 -0300

    added IOrderRepository interface to define contracts for order data persistence.

---
+++ Source/Data/Repositories/Interfaces/IOrderRepository.cs
@@ -0,0 +1,6 @@
+namespace Comanda.WebApi.Data.Repositories;
+
+public interface IOrderRepository : IMinimalRepository<Order>
+{
+    /* This interface inherits the contracts from IMinimalRepository. */
+}

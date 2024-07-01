093ba5bd633847febcfef3a1d397902c27cf1a98
https-richardy <code.richardy@gmail.com>
2024-07-01 11:59:33 -0300

    added new batch migrations.

---
+++ Source/Data/Migrations/20240701145908_BatchFieldUpdates.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Comanda.WebApi.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class BatchFieldUpdates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Establishments_EstablishmentId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "OrderItems");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EstablishmentId",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PaymentMethod",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ShippingAddressId",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_ShippingAddressId",
+                table: "Orders",
+                column: "ShippingAddressId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Addresses_ShippingAddressId",
+                table: "Orders",
+                column: "ShippingAddressId",
+                principalTable: "Addresses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Establishments_EstablishmentId",
+                table: "Products",
+                column: "EstablishmentId",
+                principalTable: "Establishments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Addresses_ShippingAddressId",
+                table: "Orders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Establishments_EstablishmentId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_ShippingAddressId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentMethod",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "ShippingAddressId",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EstablishmentId",
+                table: "Products",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "OrderItems",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Establishments_EstablishmentId",
+                table: "Products",
+                column: "EstablishmentId",
+                principalTable: "Establishments",
+                principalColumn: "Id");
+        }
+    }
+}
